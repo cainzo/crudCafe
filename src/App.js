18f6fa448e4bf4a9c8eeffe98bb2ad88ef759333
GonzaloC <cainzog@gmail.com>
2021-11-23 09:46:47 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 
+
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Inicio from './components/pages/Inicio';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListaProductos from './components/productos/ListaProductos';
+import AgregarProducto from './components/productos/AgregarProducto';
+import EditarProducto from './components/productos/EditarProducto';
+import Navigation from './components/common/Navigation';
+import Footer from './components/common/Footer';
+import Error404 from './components/pages/Error404';
+
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<Inicio></Inicio>}></Route>
+        <Route path='/productos' element={<ListaProductos></ListaProductos>}></Route>
+        <Route path='/productos/nuevo' element={<AgregarProducto></AgregarProducto>}></Route>
+        <Route path='/productos/editar' element={<EditarProducto></EditarProducto>}></Route>
+        <Route path='*' element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
